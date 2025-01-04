be531acb2cac8c2b22cf50b52db1fa8fdc8bfb0b
Ikhlasul Amal <54785909+ikhlasul0507@users.noreply.github.com>
2025-01-04 07:39:18 +0700

    upgrade migrate

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -252,5 +252,23 @@ class Migrate extends CI_Controller
 			echo "||............[Migrate successfully " . $title . "]</br>";
 		}
 		//=================================================================================================
+		//=================================================================================================
+		$column = "question_type";
+		$table_name = "m_questions";
+		$title = "Add Column " . $column . " to table " . $table_name;
+		$query = "SELECT COUNT(*) as count FROM information_schema.columns WHERE table_schema=DATABASE() AND table_name= '" . $table_name . "' AND column_name = '" . $column . "'";
+		$check = $this->db->query($query)->first_row('array');
+		if ($check['count'] == '0') {
+			$queryAlter = "ALTER TABLE $table_name
+			ADD $column CHAR(1) DEFAULT 'C';";
+			if ($this->db->query($queryAlter)) {
+				echo "||............[Migrate successfully " . $title . "]</br>";
+			} else {
+				echo "||............[Migrate failed " . $title . "]</br>";
+			}
+		} else {
+			echo "||............[Migrate successfully " . $title . "]</br>";
+		}
+		//=================================================================================================
 	}
 }
